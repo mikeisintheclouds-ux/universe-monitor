044ce431311d981dab988aa20ae3c25588df082e
mikeisintheclouds-ux <mikeisintheclouds@gmail.com>
2026-08-11 05:56:57 -0400

    wire: Scout/PHA/SBDB into snapshot + UI + styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { getUniverseSnapshot } from "@/lib/data";
 import { MonitorClient } from "@/components/MonitorClient";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 1800;
 
-export default async function Page() {
+export default async function HomePage() {
   const snap = await getUniverseSnapshot();
-
   return (
-    <div className="shell">
+    <main className="shell">
+      <header className="topbar">
+        <div className="brand">
+          <h1>Universe Monitor</h1>
+          <div className="sub">Space situational awareness console</div>
+        </div>
+      </header>
       <MonitorClient
         planets={snap.planets}
         iss={snap.iss}
@@ -18,9 +23,15 @@ export default async function Page() {
         starlink={snap.starlink}
         epic={snap.epic}
         cad={snap.cad}
+        phaCad={snap.phaCad}
+        scout={snap.scout}
         sentry={snap.sentry}
+        apod={snap.apod}
+        eonet={snap.eonet}
+        nasaTle={snap.nasaTle}
+        ssc={snap.ssc}
         generatedAt={snap.generatedAt}
       />
-    </div>
+    </main>
   );
 }
